9626c7f5adb6c130712edccf5d7a349bd293220b
shawn mcallister <Shawnmcallister39@gmail.com>
2021-02-01 22:16:52 -0800

    Routed search input data to main component.

--- src/Component/SearchBar.js
+++ src/Component/SearchBar.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BiSearch } from 'react-icons/bi';
 
-const SearchBar = () => {
+const SearchBar = props => {
+  const [inputData, setInputData] = useState('');
+
+  const handleInputChange = event => {
+    setInputData(event.target.value);
+  }
+
+  const handleSubmit = event => {
+    props.inputData(inputData);
+    setInputData('');
+    event.preventDefault();
+  }
+
   return(
-    <div className='flex w-full h-12 bg-white rounded-md'>
-      <input className='w-full h-full bg-white rounded-l-md shadow-inner p-2 focus:outline-none'>
+    <form className='flex w-full h-12 bg-white rounded-md' onSubmit={handleSubmit}>
+      <input className='w-full h-full bg-white rounded-l-md shadow-inner py-2 px-3 focus:outline-none' value={inputData} onChange={handleInputChange}>
       </input>
       <button className='flex justify-center items-center w-20 bg-green-400 rounded-r-md text-white text-xl hover:bg-green-500 shadow-inner focus:outline-none'>
         <BiSearch />
       </button>
-    </div>
+    </form>
   );
 }
 
--- src/Component/SearchOutput.js
+++ src/Component/SearchOutput.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SearchOutput = () => {
   return(
-    <div>
+    <div className='w-full h-auto mt-10 bg-white flex-grow rounded-md shadow-inner overflow-y-auto'>
 
     </div>
   );
